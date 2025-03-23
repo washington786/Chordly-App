be4ac4618f7bcdded2792e93dda837ce654c58f0
washington786 <dkmawasha@gmail.com>
2025-03-23 04:40:04 +0200

    cors settings

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,49 +1,3 @@
-// import "module-alias/register";
-// import "gitignore";
-// import express, { Request, Response } from "express";
-// import "dotenv/config";
-
-// import "#/db/index";
-// // import "./db/index";
-
-// import { PORT } from "#/utils/variables";
-// import authRouter from "#/routers/auth";
-// import audio from "./routers/audio";
-// import favorites from "./routers/favorites";
-// import playlistRouter from "./routers/playlist";
-// import profileRouter from "./routers/profile";
-// import historyRouter from "./routers/history";
-
-// import "#/utils/cron";
-// import { errorHandler } from "./middleware/error";
-
-// const app = express();
-
-// app.use(express.json());
-
-// app.use(express.static("src/public"));
-
-// app.use(express.urlencoded({ extended: true }));
-// app.get("/", (req:Request, res:Response) => {
-//   res.send("Server is running!")
-// });
-// app.use("/auth", authRouter);
-// app.use("/audio", audio);
-// app.use("/favorites", favorites);
-// app.use("/playlist", playlistRouter);
-// app.use("/profile", profileRouter);
-// app.use("/history", historyRouter);
-
-// app.use(errorHandler);
-
-// app
-//   .listen(PORT)
-//   .on("listening", function () {
-//     console.log(`Server is running on port ${PORT}`);
-//   })
-//   .on("error", function (err: Error) {
-//     console.log(err.message);
-//   });
 import "module-alias/register"; // Resolve path aliases at runtime
 import "dotenv/config"; // Load environment variables from .env file
 
@@ -59,6 +13,7 @@ import favorites from "./routers/favorites";
 import playlistRouter from "./routers/playlist";
 import profileRouter from "./routers/profile";
 import historyRouter from "./routers/history";
+import cors from "cors";
 
 // Utilities
 import { PORT } from "#/utils/variables"; // Ensure this file exports the PORT variable
@@ -75,7 +30,8 @@ app.use(express.urlencoded({ extended: true })); // Parse URL-encoded request bo
 
 // Serve static files from the "src/public" directory
 app.use(express.static("src/public"));
-
+app.use(cors());
+app.options("*", cors());
 // Routes
 app.get("/", (req: Request, res: Response) => {
   res.send("Server is running!");
